f9404fb9574aaa5469c1d2ddf22a8d875b168eb7
saifulmahbub <saifulislammahbub83@gmail.com>
2023-06-07 16:22:00 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Subject name="Saiful's" topic="arabic" like="English"></Subject>
+      <Subject name="rakib's" topic="Bangla" like="Arabic"></Subject>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+    </div>
+  );
+}
+
+function Subject(props) {
+  return (
+    <div>
+      <h2>
+        {props.name} favourite Subject is {props.topic}
+      </h2>
+      <h3>
+        {" "}
+        {props.name} also loves {props.like}
+      </h3>
     </div>
   );
 }
